8d7dfb26b126f57873c487b0f2ce98e6a86caac5
TJ E <thomasjayellis@gmail.com>
2014-03-15 20:20:43 -0400

    Added AlbumInfoViewer control for displaying AlbumInfo objects. Added MockAlbumInfo to mockup content for viewing in the designer

---
+++ MusicUnpacker/MockAlbumInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicUnpacker
+{
+    public class MockAlbumInfo : AlbumInfo
+    {
+        public MockAlbumInfo()
+            : base()
+        {
+            Title = "Disintegration";
+            Artist = "The Cure";
+            Genre = "Awesome";
+        }
+    }
+}
